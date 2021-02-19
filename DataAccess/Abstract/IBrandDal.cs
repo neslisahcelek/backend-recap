83357fc1ca8f6f8b39250ba27274ce15fc106689
Neslişah Çelek <neslisah.celek@outlook.com>
2021-02-19 19:51:56 +0300

    19 Feb

--- DataAccess/Abstract/IBrandDal.cs
+++ DataAccess/Abstract/IBrandDal.cs
@@ -5,9 +5,8 @@ using System.Text;
 
 namespace DataAccess.Abstract
 {
-    public interface ICarDal : IEntityRepository<Car>
+    interface IBrandDal : IEntityRepository<Brand>
     {
-    
-
+        
     }
 }
